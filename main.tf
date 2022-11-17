49b7440bf867c531e5f79ab2c3dceff9d2627c3b
yuta <yoshinagayuuta@outlook.jp>
2022-11-17 10:53:47 +0900

    雛形作成

---
+++ main.tf
@@ -0,0 +1,31 @@
+# -------------
+# terraform configration
+# -------------
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+# -------------
+# providers
+# -------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+# -------------
+# variables
+# -------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
